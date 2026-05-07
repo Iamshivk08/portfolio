e5c5695e745ea183ac6c4e7d58f6eb607d42e090
Shiv K <shivkatam08@gmail.com>
2026-05-07 15:25:01 +0530

    Add files via upload

---
+++ portifilio.js
@@ -0,0 +1,12 @@
+function fun() {
+    window.location.assign("C:\\Users\\Asus\\OneDrive\\Desktop\\web8\\JavaScript\\calculator\\calculator.html");
+}
+function fun1() {
+    window.location.assign("C:\\Users\\Asus\\OneDrive\\Desktop\\web8\\CSS\\flipkart.html");
+}
+function studentProject() {
+    alert("This project manages student records like add, update, delete and view.");
+}
+function contactMsg() {
+    alert("Thanks for visiting my portfolio!");
+}
